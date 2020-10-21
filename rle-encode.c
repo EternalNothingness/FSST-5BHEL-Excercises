02fbcf7aff09a5701978ef0f7efbd50255240fdf
EternalNothingness <66483318+EternalNothingness@users.noreply.github.com>
2020-10-21 15:31:21 +0200

    Update rle-encode.c

--- rle-encode.c
+++ rle-encode.c
@@ -3,10 +3,11 @@
 
 char *encode(char *str)
 {
+	char *result_start;
 	char *result;
+	result = result_start;
 	char temp = *str;
 	int cnt_char = 1;
-	int cnt_result = 0;
 
 	for(; str != "\0"; str++)
 	{
@@ -16,23 +17,17 @@ char *encode(char *str)
 		}
 		else
 		{
-			result = result + cnt_result;
 			*result = temp;
-			cnt_result++;
-			result = result + cnt_result;
-			*result = "%i", cnt_char;
-			cnt_result++;
+			*(++result) = "%i", cnt_char;
+			result++;
 			temp = *str;
 			cnt_char = 1;
 		}	
 	}
-	result = result + cnt_result;
 	*result = temp;
-	cnt_result++;
-	result = result + cnt_result;
-	*result = "%i", cnt_char;
+	*(++result) = "%i", cnt_char;
 
-	return *result;
+	return *result_start;
 }
 
 int main()
